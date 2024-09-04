6bdd9e5948e1fd5ca2dcf94bcc468b4959c21ba7
bettoo <joaco_fer@yahoo.com.ar>
2024-09-04 00:47:42 -0300

    other changes

---
+++ victory_checker/victory_checker.py
@@ -0,0 +1,8 @@
+class VictoryChecker():
+    def __init__(self, victory_patterns: list):
+        self.__victory_patterns = victory_patterns
+
+    def check_combinations(self, combinations):
+        pass
+
+    def get_combinations_by_last_move(self, tablero: TableroLogica, last_move)
