808c3692de3c084386209787cc23eee20614a486
John Doe <johndoe@example.com>
2024-09-10 22:06:31 -0300

    First turn is the team with the 'FichaCruz'

--- turno_manager/team_oriented_turno_manager.py
+++ turno_manager/team_oriented_turno_manager.py
@@ -1,3 +1,4 @@
+from tablero.objects.ficha_tateti.ficha_cruz import FichaCruz
 from turno_manager.turno_manager import TurnoManager
 from team.teams import Team
 
@@ -5,7 +6,7 @@ class TeamOrientedTurnoManager(TurnoManager):
     def __init__(self, teams: list[Team]):
         super().__init__(teams)
         self.__teams = teams
-        self.__team_index = 0
+        self.__team_index = self.__get_team_turn_with_ficha_cruz(teams)
         self.__player_index = {team_index: 0 for team_index in range(len(teams))}
 
     def get_player_turn(self):
@@ -14,6 +15,12 @@ class TeamOrientedTurnoManager(TurnoManager):
 
     def get_team_turn(self):
         return self.__teams[self.__team_index]
+    
+    def __get_team_turn_with_ficha_cruz(self, teams):
+        for team in teams:
+            if isinstance(team._TatetiTeam__ficha, FichaCruz):
+                return teams.index(team)
+        return 0 # Si no hay ficha cruz entonces toma el primer equipo
 
     def next_turn(self):
         ultimo_equipo = self.__team_index == len(self.__teams) - 1 # Condition
